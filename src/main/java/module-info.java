028b58533555fa864033f6f9b0f9d64cd57e85ff
Kalana-maduranga001 <kalanamadu2000@gmail.com>
2025-04-20 19:41:17 +0530

    folderStructhur

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module lk.ijse.gdse.project.hibernate_project {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
 
 
     opens lk.ijse.gdse.project.hibernate_project to javafx.fxml;
